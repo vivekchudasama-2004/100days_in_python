e5873ec2ea3edb286e9895b5554ccec6a5ad2da0
Vivek Chudasama <127607552+vivekchudasama-2004@users.noreply.github.com>
2024-11-10 11:50:24 +0530

    Delete 100days_in_python/day16 directory

--- 100days_in_python/day16/importing module.py
+++
@@ -1,9 +0,0 @@
-from prettytable import PrettyTable
-#object=vlass()
-table = PrettyTable()
-#calling an method
-table.add_column("pockemon", ["pikachu", "squirtle", "charmender"])
-table.add_column("type", ["electric", "water", "fire"])
-#changing the attributr / variable
-table.align="l"
-print(table)
